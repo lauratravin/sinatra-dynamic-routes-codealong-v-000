117c073ac1236fe39c447f8c969de1efab8583a9
Laura Messa Travin <laura.travin@gmail.com>
2019-03-15 22:01:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  get "/multiply/:name" do
+  get "/multiply/:num1/:num2" do
     @user_name = params[:name]
     "Goodbye, #{@user_name}."
   end
